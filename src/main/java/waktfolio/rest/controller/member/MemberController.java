fb3bd0d15f6d2696b6a3e1c617960b2c853226ee
seunhoo <seunhoo@naver.com>
2024-04-17 14:57:52 +0900

    Update MemberController.java

--- src/main/java/waktfolio/rest/controller/member/MemberController.java
+++ src/main/java/waktfolio/rest/controller/member/MemberController.java
@@ -1,5 +1,6 @@
 package waktfolio.rest.controller.member;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,24 +13,29 @@ import waktfolio.rest.dto.member.*;
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
+@Tag(name = "멤버 관리")
 public class MemberController {
     private final MemberService memberService;
     @PostMapping("/login")
+    @Tag(name = "로그인")
     public ResponseEntity<ApiResponse> loginMember(HttpServletRequest request, @RequestBody LoginMemberRequest loginMemberRequest){
         LoginMemberResponse login = memberService.login(loginMemberRequest);
         return new ResponseEntity<>(ApiResponse.of(request,login),HttpStatus.OK);
     }
     @PostMapping("")
+    @Tag(name = "회원가입")
     public ResponseEntity<ApiResponse> registerMember(HttpServletRequest request, @RequestBody RegisterMemberRequest registerMemberRequest){
         memberService.register(registerMemberRequest);
         return new ResponseEntity<>(ApiResponse.of(request),HttpStatus.OK);
     }
     @PatchMapping("")
+    @Tag(name = "계정 정보 업데이트")
     public ResponseEntity<ApiResponse> updateMember(HttpServletRequest request, @RequestBody UpdateMemberRequest updateMemberRequest){
         memberService.update(request, updateMemberRequest);
         return new ResponseEntity<>(ApiResponse.of(request),HttpStatus.OK);
     }
     @GetMapping("")
+    @Tag(name = "내 정보 조회")
     public ResponseEntity<ApiResponse> getProfile(HttpServletRequest request){
         MemberProfileResponse profile = memberService.profile(request);
         return new ResponseEntity<>(ApiResponse.of(request, profile),HttpStatus.OK);
